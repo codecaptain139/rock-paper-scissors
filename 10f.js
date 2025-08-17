c0f688c90d28da34fe1a0e8cb14db2a8e819da6b
adarshchauhan139 <adarshchauhan54321@gmail.com>
2025-08-17 09:40:00 +0530

    first commit

---
+++ 10f.js
@@ -0,0 +1,8 @@
+function toggleButton(selector) {
+    const btn = document.querySelector(selector);
+    if(btn.classList.contains('is-toggled')) {
+            btn.classList.remove('is-toggled')
+        } else {
+            btn.classList.add('is-toggled')
+        }
+} 
